10c56f9f06b8159aa5d26d6e846213597ae5e87b
yzm <315793076@qq.com>
2021-05-05 00:48:43 +0800

    init

---
+++ src/renderer/views/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="home">
+    <img
+      alt="Vue logo"
+      src="../assets/logo.png"
+    >
+    你好世界，好不好
+    <button @click="increase">
+      加1s
+    </button>
+    <button @click="reset">
+      reset
+    </button>
+    <button @click="getBaidu">
+      getBaidu
+    </button>
+    <HelloWorld :msg="`Welcome to Your Vue.js App ${index}`" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import HelloWorld from "@/components/HelloWorld";
+import axios from 'axios';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      index: 0
+    }
+  },
+  created() {
+    console.log('home ')
+  },
+  methods: {
+    getBaidu() {
+      axios.get('https://www.baidu.com').then(res => {
+        console.log(res)
+      })
+    },
+    increase() {
+      this.index++
+    },
+    reset() {
+      this.index = 0
+      // eslint-disable-next-line no-debugger
+      debugger
+    }
+  }
+}
+</script>
